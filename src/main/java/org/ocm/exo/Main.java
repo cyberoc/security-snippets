e3dc83a1c28e24ca7263a1e740a5324d863132ea
lando <cyberoc_1@hotmail.com>
2021-07-15 09:53:09 -0700

    Adding more examples

--- src/main/java/org/ocm/exo/Main.java
+++ src/main/java/org/ocm/exo/Main.java
@@ -1,6 +1,7 @@
 package org.ocm.exo;
 
 import org.ocm.exo.utils.InputValidation;
+import org.ocm.exo.utils.StringValidation;
 
 public class Main {
 
@@ -16,5 +17,14 @@ public class Main {
 
         InputValidation.validateFutureDay(2020, 12, 12);
         InputValidation.validateFutureDay(2020, 1, 1);
+
+        InputValidation.validateEmail("amazon@amazon.com");
+        InputValidation.validateEmail("amazon@amazon@aws.com");
+
+        StringValidation.validateLength("Test");
+        StringValidation.validateLength("本田茂");
+
+        StringValidation.regExFileName("file&name");
+        StringValidation.regExFileName("file-name.zip");
     }
 }
